498e8a44ed2a147e1b72fa47d464d236e8b57770
edengi1 <74146562+edengi1@users.noreply.github.com>
2021-04-04 19:47:07 +0300

    Update main1_2.c

--- task_a/main1_2.c
+++ task_a/main1_2.c
@@ -1,10 +1,8 @@
 #include "hello_ariel.h"
 #include <stdlib.h>
 
-int main() {
-
-    // int export_command = system("export LD_LIBRARY_PATH=.");
-    // printf("export_command = %d\n", export_command);
-    welcome("Tal");
+int main() 
+{
+    welcome("adi");
     return 0;
 }
